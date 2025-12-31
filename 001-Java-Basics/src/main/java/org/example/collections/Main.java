d3d4e6f3bec9d83efc14e59e28ebaec524a6e02b
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-12-31 13:47:32 -0500

    saw Sets and Queues, pending Maps and some theory talk

--- 001-Java-Basics/src/main/java/org/example/collections/Main.java
+++ 001-Java-Basics/src/main/java/org/example/collections/Main.java
@@ -1,12 +1,14 @@
 package org.example.collections;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("=============(Lists)");
+        System.out.println("=================================(Lists)");
 
         //ArrayList - one of the most general purpose collections. Very Array-ish
 
@@ -22,6 +24,75 @@ public class Main {
 
         //Print out the List of Pokemon
         System.out.println(pokemonList);
+
+        //We can access specific elements of the List with the .get() method
+        System.out.println("First Pokemon: " + pokemonList.get(0));
+        //size() returns an int representing the number of elements in the List
+        System.out.println("Last Pokemon: " + pokemonList.get(pokemonList.size() - 1));
+
+        //We can use the set() method to change an element by its index
+        pokemonList.set(0, new Pokemon("Swampert", 40));
+
+        //forEach() allows us to execute some logic FOR EACH element in the List
+        //notice the syntax here (pokemon -> ...) this is called a "lambda". It's like a shorthand method
+        pokemonList.forEach(pokemon -> pokemon.fight(new Pokemon("Magikarp", 1)));
+
+        System.out.println("=============================================(Sets)");
+
+        //HashSet - A general-purpose set, no crazy rules besides the rules of a set
+        //(No duplicates, no defined order, no index, hence no get() method)
+
+        HashSet<Pokemon> pokemonSet = new HashSet<>();
+
+        //add a few Pokemon including a duplicate
+        pokemonSet.add(new Pokemon("Pikachu", 25));
+        pokemonSet.add(new Pokemon("Pikachu", 25));
+        pokemonSet.add(new Pokemon("Snorlax", 100));
+        pokemonSet.add(new Pokemon("Jigglypuff", 20));
+
+        //Sets don't allow duplicates...
+        System.out.println(pokemonSet);
+        //So why are there two Pikachus?? We used the "new" keyword so the Pikachus are identical, but different objects
+
+        //Let's define and add two of the same Pokemon object
+        Pokemon mew = new Pokemon("Mew", 70);
+        pokemonSet.add(mew);
+        pokemonSet.add(mew);
+
+        //Print it out again - only one Mew! Also, notice the arbitrary order
+        System.out.println(pokemonSet);
+
+        //Remember, no get() since there's no index
+        //pokemonSet.get(0);
+
+        //We CAN use contains() to see if a certain element exists in the Set
+        if(pokemonSet.contains(mew)){
+            System.out.println("The Set contains() Mew");
+        }
+
+
+        System.out.println("==========================================(Queues)");
+
+        //LinkedList - A Collection that implements both List AND Queue
+        //Queues follow FIFO (First In First Out) ordering, so we can only access/remove the element at the 0th index
+
+        LinkedList<Pokemon> pokemonQueue = new LinkedList<>();
+
+        //Add some Pokemon to the Queue
+        pokemonQueue.add(new Pokemon("Bulbasaur", 5));
+        pokemonQueue.add(new Pokemon("Squirtle", 5));
+        pokemonQueue.add(new Pokemon("Charmander", 5));
+
+        System.out.println("Welcome to the Pokemon Center: " + pokemonQueue);
+
+        //peek() allows us to get the first element, but doesn't manipulate it
+        System.out.println("The next Pokemon in line is: " + pokemonQueue.peek().getName());
+
+        //poll() allows us to get AND remove the first element
+        System.out.println("Now serving: " + pokemonQueue.poll().getName());
+
+        //one last peek just to prove the first element was removed
+        System.out.println("The next Pokemon in line is: " + pokemonQueue.peek().getName());
 
     }
 
